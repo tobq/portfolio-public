716670656db0d911de3c447b5f8c19463b5b9c10
tobi <tobi.akiny@gmail.com>
2019-06-02 13:07:01 +0100

    push

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ $str = "/index.html";
 switch (explode('.', $_SERVER['HTTP_HOST'])[0]) {
     case "lope":
         $str = "https://www.instagram.com/lopelocks";
+        break;
     case "paul":
         $str = "https://www.linkedin.com/in/paul-akinyemi-9b529776";
+        break;
 };
 
 
