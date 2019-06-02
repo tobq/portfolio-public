dbec98a224b49155ca3542a2fac4bcaec0991e2d
tobi <tobi.akiny@gmail.com>
2019-06-02 13:02:51 +0100

    push

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo $_SERVER['HTTP_HOST'];
+$str = "/index.html";
+switch (explode('.', 'en.example.com')[0]) {
+    case "lope":
+        $str = "https://www.instagram.com/lopelocks";
+    case "paul":
+        $str = "https://www.linkedin.com/in/paul-akinyemi-9b529776";
+};
 
-//header( 'Location: /index.html' ) ;
+
+header("Location: $str");
