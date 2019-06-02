7cee779304870781aba1b1592da62fe59b674c94
tobi <tobi.akiny@gmail.com>
2019-06-02 16:21:40 +0100

    linking

--- index.php
+++ index.php
@@ -10,8 +10,11 @@ switch (explode('.', $_SERVER['HTTP_HOST'])[0]) {
     case "cv":
         $str = "https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg";
         break;
-    default:
-        $str = "/index.html";
+    case "tobi":
+        echo file_get_contents('index.html');
+        break;
+    default :
+        $str = "http://tobi.akinyemi.uk";
 };
 
 
