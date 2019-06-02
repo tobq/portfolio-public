5d58473f7e961eacc59924de3d011b6b7a971e32
tobi <tobi.akiny@gmail.com>
2019-06-02 15:49:13 +0100

    added cv link

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-$str = "/index.html";
 switch (explode('.', $_SERVER['HTTP_HOST'])[0]) {
     case "lope":
         $str = "https://www.instagram.com/lopelocks";
@@ -8,6 +7,11 @@ switch (explode('.', $_SERVER['HTTP_HOST'])[0]) {
     case "paul":
         $str = "https://www.linkedin.com/in/paul-akinyemi-9b529776";
         break;
+    case "cv":
+        $str = "https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg";
+        break;
+    default:
+        $str = "/index.html";
 };
 
 
