141092d0f0bc324189468998e38dbf04d0b9a9be
tobi <tobi.akiny@gmail.com>
2019-06-05 19:51:03 +0100

    updated link

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 
 switch (explode('.', $_SERVER['HTTP_HOST'])[0]) {
     case "lope":
-        $str = "https://www.instagram.com/lopelocks";
+        header("Location: https://www.instagram.com/lopelocks");
         break;
     case "paul":
-        $str = "https://www.linkedin.com/in/paul-akinyemi-9b529776";
+        header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
         break;
     case "cv":
-        $str = "https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg";
+        header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
         break;
     case "tobi":
         echo file_get_contents('index.html');
         break;
     default :
-        $str = "http://tobi.akinyemi.uk";
+        header("Location: http://tobi.akinyemi.uk");
 };
 
 
-header("Location: $str");
