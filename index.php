1ff0bf571e4cb94deafa5621dd9137fe152f7085
tobi akinyemi <tobi.akiny@gmail.com>
2019-10-21 14:52:40 +0100

    updated subdomain shit

--- index.php
+++ index.php
@@ -6,20 +6,20 @@ $DOMAIN_LENGTH = strlen($DOMAIN);
 $domain = $_SERVER['HTTP_HOST'];
 
 $subdomain = substr($domain, 0, strlen($domain) - $DOMAIN_LENGTH - 1);
-
-switch ($subdomain) {
-    case "lope":
-        header("Location: https://www.instagram.com/lopelocks");
-        break;
-    case "paul":
-        header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
-        break;
-    case "cv":
-        header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
-        break;
-    case "tobi":
-        echo file_get_contents('index.html');
-        break;
-    default:
-        header("Location: http://tobi.$DOMAIN");
-};
+echo $subdomain
+//switch ($subdomain) {
+//    case "lope":
+//        header("Location: https://www.instagram.com/lopelocks");
+//        break;
+//    case "paul":
+//        header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
+//        break;
+//    case "cv":
+//        header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
+//        break;
+//    case "tobi":
+//        echo file_get_contents('index.html');
+//        break;
+//    default:
+//        header("Location: http://tobi.$DOMAIN");
+//};
