f1ce866e1423f4f0d997a641738d70335d505e7b
tobi akinyemi <tobi.akiny@gmail.com>
2019-10-21 16:37:54 +0100

    updated subdomain shit

--- index.php
+++ index.php
@@ -17,22 +17,22 @@ function redirect($DOMAIN)
 
 echo $domain;
 
-if (substr_compare($domain, $DOMAIN, -strlen($DOMAIN)) !== 0) {
-    echo ">>>>>>>>>>>>>>>>>>>>>>>";
-    redirect($DOMAIN);
-} else switch ($subdomain) {
-        case "lope":
-            header("Location: https://www.instagram.com/lopelocks");
-            break;
-        case "paul":
-            header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
-            break;
-        case "cv":
-            header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
-            break;
-        case "tobi":
-            echo file_get_contents('index.html');
-            break;
-        default:
-            redirect($DOMAIN);
-    };
+//if (substr_compare($domain, $DOMAIN, -strlen($DOMAIN)) !== 0) {
+//    echo ">>>>>>>>>>>>>>>>>>>>>>>";
+//    redirect($DOMAIN);
+//} else switch ($subdomain) {
+//        case "lope":
+//            header("Location: https://www.instagram.com/lopelocks");
+//            break;
+//        case "paul":
+//            header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
+//            break;
+//        case "cv":
+//            header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
+//            break;
+//        case "tobi":
+//            echo file_get_contents('index.html');
+//            break;
+//        default:
+//            redirect($DOMAIN);
+//    };
