996854cd4883bdb69bf2af94451166bb7127e373
tobi akinyemi <tobi.akiny@gmail.com>
2019-10-21 14:51:12 +0100

    updated domain shit

--- index.php
+++ index.php
@@ -5,23 +5,21 @@ $DOMAIN_LENGTH = strlen($DOMAIN);
 
 $domain = $_SERVER['HTTP_HOST'];
 
-echo substr($domain, 0, strlen($domain) - $DOMAIN_LENGTH);
-//
-//switch (substr($domain, 0, strlen($domain) - $DOMAIN_LENGTH)) {
-//    case "lope":
-//        header("Location: https://www.instagram.com/lopelocks");
-//        break;
-//    case "paul":
-//        header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
-//        break;
-//    case "cv":
-//        header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
-//        break;
-//    case "tobi":
-//        echo file_get_contents('index.html');
-//        break;
-//    default:
-//        header("Location: http://tobi.$DOMAIN");
-//};
-//
-//
+$subdomain = substr($domain, 0, strlen($domain) - $DOMAIN_LENGTH - 1);
+
+switch ($subdomain) {
+    case "lope":
+        header("Location: https://www.instagram.com/lopelocks");
+        break;
+    case "paul":
+        header("Location: https://www.linkedin.com/in/paul-akinyemi-9b529776");
+        break;
+    case "cv":
+        header("Location: https://1drv.ms/w/s!AnLTraYGkdweklQBhTZ2c8wWwEJg");
+        break;
+    case "tobi":
+        echo file_get_contents('index.html');
+        break;
+    default:
+        header("Location: http://tobi.$DOMAIN");
+};
